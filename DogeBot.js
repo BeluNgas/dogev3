8945d3e5099385ac944c8e780a69b59b77f82de5
BelluNgas <98247916+BeluNgas@users.noreply.github.com>
2022-03-07 18:41:20 +0800

    Update DogeBot.js

--- DogeBot.js
+++ DogeBot.js
@@ -625,7 +625,7 @@ TQ TO ADIWAJSHING
 TQ TO ALL
 
 teks =
-`_Please Select Button Below_
+`Please Select Button Below
 _If You Are A Mod User_
 _Please Type ${prefix}command_`
 DogeXeonOP.sendMessage(from, { contentText: `${teks}`, footerText: `${menu}`, buttons: [{ buttonId: `${prefix}command`, buttonText: { displayText: '️❐𝑀𝐸𝑁𝑈' }, type: 1 },{ buttonId: `${prefix}sc`, buttonText: { displayText: 'SCRIPT 📝' }, type: 1 },{ buttonId: `${prefix}developer`, buttonText: { displayText: 'DEVELOPER 👨🏼‍💻' }, type: 1 } ], headerType: 'LOCATION', locationMessage: { degreesLatitude: '', degreesLongitude: '', jpegThumbnail: fakeimage, contextInfo: {mentionedJid: [senderr,pemilik]}}}, 'buttonsMessage')
